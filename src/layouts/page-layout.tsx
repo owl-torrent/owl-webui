5a63105d17549cfd8cbebc5e97dff0422351ea8c
Anthony RAYMOND <anthonyraymond@users.noreply.github.com>
2021-04-22 22:44:50 +0200

    fix drawer content and alignement

--- src/layouts/page-layout.tsx
+++ src/layouts/page-layout.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { AppBar, IconButton, Toolbar, List, ListItem, ListItemText, ListItemIcon, Hidden } from '@material-ui/core'
-import { Dashboard as DashboardIcon, Menu as MenuIcon, Subject as SubjectIcon, Settings as SettingsIcon, UploadFile as UploadFileIcon } from '@material-ui/icons'
+import { Dashboard as DashboardIcon, Menu as MenuIcon, Subject as SubjectIcon, Settings as SettingsIcon, UploadFile as UploadFileIcon, WifiOff as WifiOffIcon } from '@material-ui/icons'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import ThemeAlterator from '../modules/theme/theme-alterator'
 import { useHistory, useRouteMatch } from 'react-router'
@@ -11,9 +11,6 @@ import clsx from 'clsx'
 import { DesktopDrawer, MobileDrawer } from './drawer'
 
 const useStyles = makeStyles((theme: Theme) => createStyles({
-  root: {
-
-  },
   flex: {
     display: 'flex',
   },
@@ -44,6 +41,12 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
     flexGrow: 1,
     paddingLeft: theme.spacing(1),
   },
+  connectToServerButton: {
+    color: theme.palette.error.main
+  },
+  offsetListIcon: {
+    paddingLeft: theme.spacing(1),
+  },
 }))
 
 interface Props {
@@ -51,36 +54,38 @@ interface Props {
 }
 
 const DrawerContent: React.FC<{}> = () => {
+  const classes = useStyles()
   const history = useHistory()
+  const { apis: { isConnected } } = useApis()
   const matchSeedRoute = useRouteMatch("/seed*")
   const matchConfigRoute = useRouteMatch("/config*")
   const matchDashboardRoute = useRouteMatch("/dashboard*")
   const matchReadTimeLogRoute = useRouteMatch("/real-time-log*")
+  const matchConnectApiRoute = useRouteMatch("/connect-api*")
 
 
   return React.useMemo(() => {
     const items = [
-      { name: "Torrents", sendTo: '/seed', match: matchSeedRoute, icon: <UploadFileIcon /> },
-      { name: "Config", sendTo: '/config', match: matchConfigRoute, icon: <SettingsIcon /> },
-      { name: "Dashboard", sendTo: '/dashboard', match: matchDashboardRoute, icon: <DashboardIcon /> },
-      { name: "Real time logs", sendTo: '/real-time-log', match: matchReadTimeLogRoute, icon: <SubjectIcon /> },
+      { name: "Torrents", sendTo: '/seed', match: matchSeedRoute, icon: <UploadFileIcon />, stompRequired: true },
+      { name: "Config", sendTo: '/config', match: matchConfigRoute, icon: <SettingsIcon />, stompRequired: true },
+      { name: "Dashboard", sendTo: '/dashboard', match: matchDashboardRoute, icon: <DashboardIcon />, stompRequired: true },
+      { name: "Real time logs", sendTo: '/real-time-log', match: matchReadTimeLogRoute, icon: <SubjectIcon />, stompRequired: true },
     ]
+    if (!isConnected) {
+      items.push({ name: "Connect to server", sendTo: '/connect-api', match: matchConnectApiRoute, icon: <WifiOffIcon className={classes.connectToServerButton} />, stompRequired: false})
+    }
 
     return <List dense>
       {items.map(item =>
-        <ListItem key={item.name} button selected={item.match !== null} onClick={() => history.push(item.sendTo)}>
-          <ListItemIcon>{item.icon}</ListItemIcon>
+        <ListItem key={item.name} disabled={item.stompRequired && !isConnected} button selected={item.match !== null} onClick={() => history.push(item.sendTo)}>
+          <ListItemIcon className={classes.offsetListIcon}>{item.icon}</ListItemIcon>
           <ListItemText primary={item.name} />
         </ListItem>
       )}
     </List>
-  }, [matchSeedRoute, matchConfigRoute, matchDashboardRoute, matchReadTimeLogRoute, history])
+  }, [isConnected, matchSeedRoute, matchConfigRoute, matchDashboardRoute, matchReadTimeLogRoute, matchConnectApiRoute, history, classes.connectToServerButton])
 }
 
-// TODO next:
-//  - Remove drawer if not connected to stomp
-//  - Center drawer icons in desktop mode. To be aligned with appbar icon menu
-//  - Change drawer "selected" style (something like gmail looks nice)
 const Layout: React.FC<Props> = (props) => {
   const { children } = props
   const classes = useStyles()
@@ -90,7 +95,7 @@ const Layout: React.FC<Props> = (props) => {
   const list = <DrawerContent />
 
   return (
-    <div className={classes.root}>
+    <div>
       <AppBar
         className={classes.appBar}
         position="sticky"
